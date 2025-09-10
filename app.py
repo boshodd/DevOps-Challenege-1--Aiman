f9fdc9f5687ab24c9f1204d53e6c9d1401f91ffc
Bosco Kuttikatt <66684265+boshodd@users.noreply.github.com>
2025-09-10 18:12:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from your deployed Python app!"
+    return "Hello from your deployed Python app. Hello again!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
